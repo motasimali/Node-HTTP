5f1bd69eab095be11f3afc5bf657c13182b2d2c8
Motasim Ali Bhatti <bitf13a011@pucit.edu.pk>
2020-06-17 16:15:40 +0500

    serving static files using fs and path modules

--- index.js
+++ index.js
@@ -1,13 +1,45 @@
 const http = require('http');
+const path = require('path');
+const fs =  require('fs');
 
 const hostname  = 'localhost';
 const port = 3000;
 
 const server = http.createServer((req,res)=>{
-    console.log(req.headers);
-    res.statusCode = 200;
-    res.setHeader('Content-Type','text/html');
-    res.end('<html><body><h1>Hello Motasim</h1></body></html>')
+    console.log('Request for '+ req.url + ' by method '+ req.method);
+    if(req.method == 'GET') {
+        var fileUrl;
+        if(req.url == '/') 
+            fileUrl = '/index.html';
+        else 
+            fileUrl = req.url;
+        var filePath = path.resolve('./public/'+fileUrl);
+        const fileExt = path.extname(filePath);
+        if(fileExt == '.html') {
+            fs.exists(filePath, (exists) => {
+                if(!exists) {
+                    res.statusCode = 404;
+                    res.setHeader('Content-Type', 'text/html');
+                    res.end('<html><body><h1>Error 404: '+ fileUrl+ ' is not found</h1></body></html>')
+                    return;
+                }
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'text/html');
+                fs.createReadStream(filePath).pipe(res);
+                
+            })
+        } else {
+            res.statusCode = 404;
+            res.setHeader('Content-Type', 'text/html');
+            res.end('<html><body><h1>Error 404: '+ fileExt+ ' format not supported</h1></body></html>')
+            return;
+        }
+    } else {
+        res.statusCode = 404;
+            res.setHeader('Content-Type', 'text/html');
+            res.end('<html><body><h1>Error 404: '+ req.method+ ' not supported</h1></body></html>')
+            return;
+    }
 });
 
 server.listen(port,hostname,()=>{
